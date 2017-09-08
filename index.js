a2eb744cadcc3bcec858d31d5f28e617b201aeb4
marcosjrs <marcosjrs80@gmail.com>
2017-09-08 20:24:58 +0200

    middleware sin parametros, con parametros y con parametros opcionales

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 'use strict'
 
-//archivo listened con nodemon, y por tanto de nuevo ejecutado con cada cambio..
-
 var express = require('express');
 var bodyparser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT ||3000;
 
-//app.use(bodyparser.urlencoded({extended:false}));
-//app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+
+//para indicar que es opcional:  app.get("/prueba/:parametro?", ...
+app.get("/prueba",function(req,resp){
+    resp.status(200).send(     //"status(200).""  no es necesario
+        {
+            texto:"Entrando en prueba sin parametros",
+            numeros:[1,2,3]
+        }
+    );
+});
+
+// capturaría por ejemplo:   http://localhost:3000/prueba/valor1 o  http://localhost:3000/prueba/valor1/valor2...
+app.get("/prueba/:parametro/:parametroOpcional?", function (req, resp) {
+    var param = req.params.parametro;
+    var param2 = req.params.parametroOpcional;
+    var texto = "Entrando con";
+    texto += req.params.parametroOpcional ? " dos parametros: " + param + " y " + param2 : " un parametro: " + param;
+    resp.send({
+        texto: texto,
+        numeros: [1, 2, 3]
+    });
+});
+
+
+
 
 
 
